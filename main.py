751fdce028b2a667944d31639415d7cc480aa920
Pedro Lucas Adorno <Pedrolucasadorno254@gmail.com>
2024-08-13 20:30:29 -0300

    Correções de compatibilidade

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
 from chatbot import ChatBot
 myChatBot = ChatBot()
 #apenas carregar um modelo pronto
@@ -12,13 +15,13 @@ myChatBot.createModel()
 print("Bem vindo ao Chatbot")
 
 
-pergunta = input("como posso te ajudar?")
+pergunta = input("como posso te ajudar?\n")
 resposta, intencao = myChatBot.chatbot_response(pergunta)
 print(resposta + "   ["+intencao[0]['intent']+"]")
 
 
 while (intencao[0]['intent']!="despedida"):
-    pergunta = input("posso lhe ajudar com algo a mais?")
+    pergunta = input("posso lhe ajudar com algo a mais?\n")
     resposta, intencao = myChatBot.chatbot_response(pergunta)
     print(resposta + "   [" + intencao[0]['intent'] + "]")
 
